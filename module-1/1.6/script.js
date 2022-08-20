defae24769cf5ffd96dfed0e51659f186ca98e4e
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-08-21 00:56:19 +0600

    counter app witb vanilla redux

--- module-1/1.6/script.js
+++ module-1/1.6/script.js
@@ -1,3 +1,9 @@
+// select dom elements
+const counterEl = document.getElementById('counter');
+const incrementEl = document.getElementById('increment');
+const decrementEl = document.getElementById('decrement');
+
+
 // initial state
 const initialState = {
     value: 0,
@@ -11,5 +17,42 @@ function counterReducer(state = initialState, action) {
             ...state,
             value: state.value + 1,
         }
+    } else if(action.type === 'decrement') {
+        return {
+            ...state,
+            value: state.value - 1
+        }
+    } else {
+        return state;
     }
 }
+
+
+// create store
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innerText = state.value.toString();
+}
+
+
+// update UI initially
+render(); 
+
+
+store.subscribe(render);
+
+
+// button click listeners
+incrementEl.addEventListener('click', () => {
+    store.dispatch({
+        type: 'increment',
+    }); 
+})
+
+decrementEl.addEventListener('click', () => {
+    store.dispatch({
+        type: 'decrement',
+    }); 
+})
