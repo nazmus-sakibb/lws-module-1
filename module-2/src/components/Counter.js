e43f21b2d756fd159369b4d909bd693b5867f330
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-08-20 15:51:20 +0600

    module 2 initiated

---
+++ module-2/src/components/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+                    <div class="text-2xl font-semibold" id="counter">0</div>
+                    <div class="flex space-x-3">
+                        <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment">
+                            Increment
+                        </button>
+                        <button class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement">
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default Counter;
